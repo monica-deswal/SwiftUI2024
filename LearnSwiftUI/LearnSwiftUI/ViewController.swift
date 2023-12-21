165470d55f7cb97860c198bd5a4890efbab8c638
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2023-12-21 20:19:26 +0530

    Example9 protocols

--- LearnSwiftUI/LearnSwiftUI/ViewController.swift
+++ LearnSwiftUI/LearnSwiftUI/ViewController.swift
@@ -7,13 +7,71 @@
 
 import UIKit
 
+protocol Printer {
+    
+    var name: String {get set}
+    func printDescription()
+}
+
+struct Employee: Printer {
+    
+    var name: String
+    var age: Int
+    
+    func printDescription() {
+        
+        print("Description: \(name) \(age)")
+    }
+}
+
+struct Offices: Printer {
+    
+    var name: String
+    var employee: Int
+    
+    func printDescription() {
+        
+        print("Description: \(name) \(employee)")
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let employee1 = Employee(name: "Steve", age: 61)
+        let office1 = Offices(name: "Mail", employee: 2)
+        
+        employee1.printDescription()
+        office1.printDescription()
+        
+        //Using protocol as data types
+        var list: [Printer] = [employee1, office1]
+        for element in list {
+            if let employe = element as? Employee {
+                print(element.age)
+            }
+            element.printDescription()
+        }
+        
+        //retreiving the protocol
+        let file = getFile(type: 1)
+        file.printDescription()
     }
-
-
+    
+    //returning values of a protocol data type
+    func getFile(type: Int) -> Printer {
+        
+        var data: Printer!
+        if type == 1 {
+            
+            data = Employee(name: "Steve", age: 61)
+        } else if type == 2 {
+            
+            data = Offices(name: "Mail", employee: 2)
+        }
+    }
 }
 
