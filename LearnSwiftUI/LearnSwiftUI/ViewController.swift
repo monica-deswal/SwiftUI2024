2c568a5c15a3b3c8338a24c78ee22d480861ed81
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2023-12-27 13:58:47 +0530

    Example11 Error

--- LearnSwiftUI/LearnSwiftUI/ViewController.swift
+++ LearnSwiftUI/LearnSwiftUI/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
+// Producing an error inside a method
+
+enum Errors: Error {
+    case OutOfStock
+}
+struct Stock {
+    
+    var totalLamps = 5
+    mutating func sold(amount: Int) throws {
+        
+        if amount > totalLamps {
+            throw Errors.OutOfStock
+        } else {
+            totalLamps = totalLamps - amount
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var myStock = Stock()
+        
+        try? myStock.sold(amount: 5)
+        
+        do {
+            try myStock.sold(amount: 8)
+        } catch {
+            print("We do not have enough lamps")
+        }
     }
 
 
