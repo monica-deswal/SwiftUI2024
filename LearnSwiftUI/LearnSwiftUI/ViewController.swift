cc9a86bacb5b2da30d55ee1c949afbd3125e209f
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2023-12-27 14:12:22 +0530

    Example12 Results

--- LearnSwiftUI/LearnSwiftUI/ViewController.swift
+++ LearnSwiftUI/LearnSwiftUI/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
+enum Errors: Error {
+    case OutOfStock
+}
+
+struct Stock {
+    
+    var totalLamps = 5
+    mutating func sold(amount: Int) -> Result<Int, Errors> {
+        
+        if amount > totalLamps {
+            return .failure(.OutOfStock)
+        } else {
+            totalLamps = totalLamps - amount
+            return .success(totalLamps)
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var myStock = Stock()
+        let result = myStock.sold(amount: 3)
+        switch result {
+            
+        case .success(let stock):
+            print("Lamps in stock: \(stock)")
+        case .failure(let error):
+            if error == .OutOfStock {
+                print("Error: Out of stock")
+            } else {
+                print("Error")
+            }
+        }
+        
+        do {
+            let stock = try result.get()
+            print("Lamps in stock: \(stock)")
+        } catch {
+            
+            print("Errors: Out of stock")
+        }
     }
 
+    
 
 }
 
