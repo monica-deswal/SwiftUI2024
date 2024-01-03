1a4903b8bc28d7ff6d6defa223fb831832a93d87
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-03 21:14:43 +0530

    Example25 Custom Views

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+/*
+ Custom layout allows us to specify the exact position of each view in 
+ */
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Grid {
+            GridRow {
+                Image(systemName: "phone")
+                    .frame(width: 100, height: 100)
+                    .background(.blue)
+                ExtractedView()
+            }
+        }.font(.title2)
+    }
+}
+
+struct ExtractedView: View {
+    var body: some View {
+        Grid(alignment: .leading) {
+            GridRow {
+                Text("My Name")
+            }
+            GridRow {
+                Text("My Number")
+            }
         }
-        .padding()
     }
 }
 
