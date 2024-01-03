9eeb852c819502bd4adcd8ff08b35630b61b3150
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-03 18:55:08 +0530

    Example22 Alignment in the view Vertical Alignment

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -7,15 +7,73 @@
 
 import SwiftUI
 
+/*
+ Alignment of the tallest view
+ */
+
+extension VerticalAlignment {
+    
+    enum BusAlignment: AlignmentID {
+        
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            return context[VerticalAlignment.center]
+        }
+    }
+    
+    static let alignBus = VerticalAlignment(BusAlignment.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack(alignment: .center) {
+            Image("image1")
+            Image("image2")
+            Image("image3")
+        }.border(Color.blue, width: 2)
+        
+        //Generate your own center alignment
+        HStack(alignment: .center) {
+            Image("image1")
+            Image("image2").alignmentGuide(VerticalAlignment.center, computeValue: { dimension in
+                return dimension[VerticalAlignment.center] + 18
+            })
+            Image("image3")
+        }.border(Color.blue, width: 2)
+        
+        //Custom alingment extension
+        HStack(alignment: .alignBus) {
+            
+            VStack {
+               Image("image2")
+            }
+            VStack(alignment: .leading) {
+              
+                Text("Mommies love")
+                Text("Beta's fun")
+                    .font(.largeTitle)
+            }
+        }.border(Color.green, width: 4)
+        
+        //Updating the center alignment
+        HStack {
+            VStack {
+                
+                Image("image3")
+                    .alignmentGuide(.bottom) { dimension in
+                     dimension[VerticalAlignment.center] - 40
+                    }
+            }
+            
+            VStack(alignment: .leading) {
+                
+                Text("Do regular excercise")
+                Text("Eat healthy")
+                    .font(.largeTitle)
+                    .alignmentGuide(VerticalAlignment.center) { dimension in
+                        dimension[VerticalAlignment.center]
+                    }
+            }
+        }.border(Color.yellow, width: 5)
     }
 }
 
