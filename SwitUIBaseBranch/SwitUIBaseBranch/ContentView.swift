213fdbc7e388be69efb8fc1da21b5bd3907362fa
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-03 20:15:34 +0530

    Example24 Implementing

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Grid {
+            GridRow {
+                Image(systemName: "message")
+                    .frame(width: 100, height: 100)
+                Image(systemName: "mic")
+                    .frame(width: 100, height: 100)
+            }.background(.cyan)
+            GridRow {
+                Image(systemName: "phone")
+                    .frame(width: 100, height: 100)
+                Image(systemName: "envelope")
+                    .frame(width: 100, height: 100)
+            }.background(.red)
         }
-        .padding()
+        
+        Spacer(minLength: 10)
+        
+        //Keeping the top text message and then the grid view
+        Grid(verticalSpacing: 10) {
+            
+            GridRow {
+                Text("Send us a message")
+            }.gridCellColumns(2)
+            GridRow {
+                Image(systemName: "phone")
+                    .frame(width: 100, height: 100)
+                Image(systemName: "envelope")
+                    .frame(width: 100, height: 100)
+            }.background(.blue)
+        }.font(.title)
+        
+        //One stack into another stack to acheive the desired result
+        Grid {
+            
+            GridRow {
+                
+                Image(systemName: "phone")
+                    .frame(width: 100, height: 100)
+                    .background(.pink)
+                
+                Grid(alignment: .leading) {
+                    GridRow {
+                        GridRow {
+                            Text("My name")
+                        }
+                        GridRow {
+                            Text("My Number")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
