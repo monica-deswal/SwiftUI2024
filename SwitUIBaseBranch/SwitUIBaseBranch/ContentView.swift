99c7086efe7f66ce86450cbe2b3d6c2d41cb6066
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-04 16:59:10 +0530

    Example28 States and two way binding

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -6,16 +6,59 @@
 //
 
 import SwiftUI
+/*
+ Bidirectional binding is used when we want to update the data in 2 way like switches we use $ sign for that.
+ */
 
 struct ContentView: View {
+    
+    @State private var title: String = "Default Title"
+    
+    @State private var validTitleState = "Valid"
+    @State private var isValid: Bool = true
+    
+    @State private var textfieldTitle: String = "Default Title"
+    @State private var titleInput: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text(title)
+                .padding(10)
+            Button(action: {
+                title = "New title"
+            }, label: {
+                Text("Change title")
+            })
+            Spacer()
+        }.padding()
+        
+        // check for the title in accordance with ternary operation
+        VStack {
+            Text(validTitleState)
+                .padding(10)
+            Button(action: {
+                isValid.toggle()
+                title = isValid ? "Valid" : "Invalid"
+            }, label: {
+                Text("Change Validation")
+            })
+            Spacer()
+        }.padding()
+        
+        //Binding property
+        VStack {
+            Text(textfieldTitle)
+                .padding(10)
+            TextField("Insert Title", text: $titleInput)
+                .textFieldStyle(.roundedBorder)
+            Button(action: {
+                textfieldTitle = titleInput
+                titleInput = ""
+            }, label: {
+                Text("Change title")
+            })
+            Spacer()
+        }.padding()
     }
 }
 
