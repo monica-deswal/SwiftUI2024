d43c95ed5a2d3dfc83c12a17d64846eaa8bcdd15
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-04 15:48:12 +0530

    Example27 Environment variable

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -6,19 +6,26 @@
 //
 
 import SwiftUI
-
+/*
+ The environment is a data structure that belongs to the application and contains data about the application and the views. It is like an external storage space accessible from anywhere in our code. The values stored in the environment can be modified or more data can be added to it.
+ */
 struct ContentView: View {
+    @Environment(\.colorScheme) var mode
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text("Hello world")
+            .foregroundStyle(Color("MyColor"))
+        
+        Image(systemName: "trash")
+            .font(Font.system(size: 100))
+            .foregroundColor(mode == .dark ? .yellow : .blue)
+            .symbolVariant(mode == .dark ? .fill : .circle)
     }
 }
 
 #Preview {
+//    ContentView()
+//        .environment(\.colorScheme, .dark)
     ContentView()
 }
