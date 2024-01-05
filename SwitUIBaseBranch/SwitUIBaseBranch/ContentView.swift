d55723c56c785095dddb3e1bdd080c87ad283ecd
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-05 08:25:42 +0530

    Example30 Binding structure(projected value and wrapped value)

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -6,19 +6,91 @@
 //
 
 import SwiftUI
+/* State Property
+ Wrapped value: This proeprty return the value managesd by the @state proeprty
+ ProjectedValue: This proeprty returns a structure of type Binding that creates the Bidirectional binding with the view.
+ */
+/* Binding property
+ WrappedValue: This property returns the value managed by the @Binding property
+ projectedValue: This property returns a structure of type Binding that creates the Bidirectional bindign with the view.
+ 
+ */
 
 struct ContentView: View {
+//    @State private var title: String = "Default Title"
+//    @State private var titleInput: String = ""
+    
+    @State private var bindingTitle: String = "DefaultTitle"
+    @State private var bindingTitleInput: String = ""
+    
+    var body: some View {
+//        VStack {
+//            Text(_title.wrappedValue)
+//                .padding(10)
+//            TextField("Insert title"
+//                      , text: _titleInput.projectedValue)
+//            .textFieldStyle(.roundedBorder)
+//            Button(action: {
+//                _title.wrappedValue = _titleInput.wrappedValue
+//                _titleInput.wrappedValue = ""
+//            }, label: {
+//                Text("Change title")
+//            })
+//            Spacer()
+//        }
+//        .padding()
+        
+        VStack {
+            HeaderView(title: $bindingTitle, titleInput: $bindingTitleInput)
+            Button(action: {
+                bindingTitle = bindingTitleInput
+                bindingTitleInput = ""
+            }, label: {
+                Text("Change title")
+            })
+            Spacer()
+        }.padding()
+        
+    }
+}
+
+struct HeaderView: View {
+   
+    
+    @Binding var title: String
+    @Binding var titleInput: String
+    let counter: Int
+    
+    init(title: Binding<String>, titleInput: Binding<String>) {
+        _title = title
+        _titleInput = titleInput
+        
+        let sentence = _title.wrappedValue
+        counter = sentence.count
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(title) (\(counter))")
+                .padding(10)
+            TextField("Insert TITLE", text: $titleInput)
+                
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+#Preview("Header") {
+    let constantTitle = Binding<String>(
+        get: { return "My Preview title"},
+        set: {value in print(value)})
+    
+    let constantInput = Binding<String>(
+        get: {return ""},
+        set: {value in print(value) })
+    
+    return HeaderView(title: constantTitle, titleInput: constantInput)
 }
