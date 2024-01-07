6335bc33e2d59b557294f3ae8dd5692044ada354
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-07 22:26:53 +0530

    Example35 Group views (a replica of stackview)

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+/*
+ We use stack views as container for other views, but SwiftUI defines a specific view for this purpose called GroupBox. This view is defined with a background color and round corners to visually group views and controls
+ */
 
 struct ContentView: View {
+    @State private var setting1: Bool = true
+    @State private var setting2: Bool = true
+    @State private var setting3: Bool = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        GroupBox("Settings") {
+            VStack(spacing: 10) {
+            Toggle("Autocorrection", isOn: $setting1)
+            Toggle("Capitalization", isOn: $setting2)
+            Toggle("Editable", isOn: $setting3)
+            }
+        }.padding()
     }
 }
 
