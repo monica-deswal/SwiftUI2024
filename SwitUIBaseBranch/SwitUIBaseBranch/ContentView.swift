e26003ad32a0c46b0d14798926b58cf68b3b9d03
monicadeswal01@gmail.com <monicafuturistic@gmail.com>
2024-01-07 22:18:51 +0530

    Example34 Steeper view plus and minus sign

--- SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
+++ SwitUIBaseBranch/SwitUIBaseBranch/ContentView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentValue: Float = 0
+    
+    @State private var currentVal: Float = 0
+    @State private var goingUp: Bool = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Text("current Value: \(currentValue.formatted(.number.precision(.fractionLength(0))))")
+            Stepper("Counter", value: $currentValue, in: 0...100)
+            Spacer()
+            
+            //stepping through 5
+            Stepper("Counter", value: $currentValue, in: 0...100, step: 5)
+            Spacer()
+            
+            //display if the value is increased or decreased
+            HStack {
+                Text("Current Value: \(currentVal.formatted(.number.precision(.fractionLength(0))))")
+                Image(systemName: goingUp ? "arrow.up" : "arrow.down")
+                    .foregroundStyle(goingUp ? .green : .red)
+                Stepper("") {
+                    currentVal += 5
+                    goingUp = true
+                } onDecrement: {
+                    currentVal -= 5
+                    goingUp = false
+                }.labelsHidden()
+                }
+
+            }
         .padding()
     }
 }
